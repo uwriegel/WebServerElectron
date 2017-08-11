32b68816ab187203129e8278443adf1fe60d3bcb
Uwe Riegel <uwriegel@gmail.com>
2017-08-11 07:09:40 +0200

    https access

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import express = require('express')
 var app = express()
 
+import fs = require('fs')
+import https = require('https')
+var options = { 
+    key: fs.readFileSync('server-key.pem'), 
+    cert: fs.readFileSync('server-crt.pem'), 
+    ca: fs.readFileSync('ca-crt.pem'), 
+}
+
 app.use(express.static('web'))
 
 app.get('/humbug', (req, res) => {
     res.send('Hello Humbug!')
 })
 
-app.listen(80, () => {
-    console.log("Webserver listening on HTTP")
+// app.listen(80, () => {
+//     console.log("Webserver listening on HTTP")
+// })
+
+https.createServer(options, app).listen(443, () => {
+    console.log("Webserver listening on HTTPS")
 })
